5e3327ec82cd0530e2ac55cf80ea4f8cf8dd7a2a
Aksh-007 <akshaybarapatre54@gmail.com>
2023-11-04 12:31:22 +0530

    Feat - database connection established

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
-const app = express();
+import dbConnection from "./config/dbConnection.js"
 
+const app = express();
+dotenv.config();
+dbConnection();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
